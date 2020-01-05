6bc234145c08f1ca73385d644755691bf107a869
Will Koeppen <will@axiomdatascience.com>
2020-01-05 11:51:17 -0800

    move from notebook to .py

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # headliner
 Scripts to pull and track headlines from news APIs.
 
-NewsAPI.org
-- I promise to add an attribution link on my website or app to NewsAPI.org.
-- 500 requests per day
+## NewsAPI.org
+This library uses the [NewsAPI.org](https://newsapi.org/) service endpoints.
+
+The API is mostly well documented, but here are some notes:
+- The free plan is limited to 500 requests per day
+- it requires attribution.
+
+NewsAPI specifically says that the API is throttled by requests, not results. I.e., ["it doesn't mantter how many results you get back"](https://newsapi.org/pricing). That may be technically true. However:
+- the results are paginated and a request will only send, at max, 20 results back.
+- the developer plan apparently only lets you see the first 100 results from a request. So you cannot see beyond page 5. When I requested data from a single source (e.g., `fox-news`) for a single day, the service said there were 111 results, but the service erros if you try to return results 100-111.
--- headliner/headliner.py
+++ headliner/headliner.py
@@ -2,12 +2,43 @@ import os
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
 
+from datetime import datetime, timedelta
+import pytz
+import json
+
 load_dotenv()
 
 API_KEY = os.getenv('NEWSAPI_KEY')
-
 api = NewsApiClient(api_key=API_KEY)
 
-srces = api.get_sources()
 
-print(srces)
+def top_headlines_now(country='us'):
+
+    now = datetime.now(pytz.utc)
+    
+    results =  api.get_top_headlines(country = 'us')
+
+    date_string = now.strftime('%Y-%m-%dT%H:%M:%S')
+
+    with open(f"headline-store-json/{date_string}-top-headlines.json", 'w') as file:
+        json.dump(results, file)
+
+
+def get_sources_on_date(begin_date, page=1):
+
+    source = "fox-news"
+    
+    end_date = begin_date + timedelta(days=1, seconds=-1)
+
+    results =  api.get_everything(
+        from_param = begin_date.strftime('%Y-%m-%dT%H:%M:%S'),
+        to = end_date.strftime('%Y-%m-%dT%H:%M:%S'),
+        language = 'en',
+        sources = source, # Max 20 sources
+        page = page
+    )
+
+    date_string = begin_date.strftime('%Y-%m-%d')
+
+    with open(f"headline-store-json/{date_string}-{source}-{page:03}.json", 'w') as file:
+        json.dump(results, file)

ad5a853b607e7ab06691f9f9cfb4d25e3f6cda32
Will Koeppen <will@axiomdatascience.com>
2020-01-09 13:19:36 -0900

    add logging and cleanup

---
+++ sendheadlines.sh
@@ -0,0 +1,10 @@
+# headlines
+git add /home/will/Projects/headliner/headline-store-json/*
+git commit -m "add yesterday's headlines"
+
+# logs
+git add /home/will/Projects/headliner/headline.log
+git commit -m "daily log"
+
+# push
+git push

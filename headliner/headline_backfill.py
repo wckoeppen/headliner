3c958be6a25dd1e9118eec69fb1d8619dbcde76f
Will Koeppen <will@axiomdatascience.com>
2020-01-16 10:02:54 -0900

    increase intervals to accommodate reuters and AP

--- headliner/headline_backfill.py
+++ headliner/headline_backfill.py
@@ -6,6 +6,7 @@ from headline_collector import get_newsapi_on_date
 from headline_processor import process_source_on_date
 import glob
 import os
+import pandas as pd
 
 logging.basicConfig(
     filename='/home/will/Projects/headliner/headliner.log',
@@ -14,35 +15,62 @@ logging.basicConfig(
     )
 logger = logging.getLogger(__name__)
 
-sources = ["abc-news"]
+sources = ["associated-press"]
 store_dir = "/home/will/Projects/headliner/datastore/raw/"
-
-# ~30 requests per day fro
-# 4 days of 2 sources = ~100 requests
-
-# 250 requests per 12 hour means gather real-time + 7 days of backfill every 12 hours should be "safe"
-# would be great to automate the backfill to run every 12 hours
-# and maybe find the max number of requests per day
 
 for source in sources:
 
-    logger.info(f"Retrieving {source} from newsapi.org")
-
-    logger.debug("Checking backdates")
-
-    filenames = sorted(glob.glob(os.path.join(store_dir, source, "*.json")))
-
-    first_file = filenames[0]
-    first_date = datetime.strptime(
-        first_file.split(source + "-")[1].split("T")[0], "%Y-%m-%d"
-    )
-
-    logger.debug(f"first date found: {first_date}")
-
-    n_days = 6
-
-    back_dates = [first_date - timedelta(days=x) for x in range(1, n_days+1)]
+    back_dates = pd.date_range(start="2020-01-13", end="2020-01-13")
 
     for back_date in back_dates:
-        get_newsapi_on_date(back_date, intervals=8, source=source)
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
+
+
+for source in sources:
+
+    back_dates = pd.date_range(start="2020-01-02", end="2020-01-03")
+
+    for back_date in back_dates:
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
+
+for source in sources:
+
+    back_dates = pd.date_range(start="2020-01-06", end="2020-01-10")
+
+    for back_date in back_dates:
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
+
+for source in sources:
+
+    back_dates = pd.date_range(start="2020-01-14", end="2020-01-15")
+
+    for back_date in back_dates:
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
+
+sources = ["reuters"]
+
+for source in sources:
+
+    back_dates = pd.date_range(start="2020-01-07", end="2020-01-09")
+
+    for back_date in back_dates:
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
+
+for source in sources:
+
+    back_dates = pd.date_range(start="2020-01-14", end="2020-01-15")
+
+    for back_date in back_dates:
+        print(back_date)
+        get_newsapi_on_date(back_date, intervals=12, source=source)
         process_source_on_date(back_date, source=source)

9bd77d41738563517ff47ff68ed2cfd3d11691b7
Will Koeppen <will@axiomdatascience.com>
2020-01-24 11:42:07 -0900

    resolve conflicts

--- headliner/headline_backfill.py
+++ headliner/headline_backfill.py
@@ -8,40 +8,21 @@ import glob
 import os
 import pandas as pd
 
-# sources = ["associated-press"]
+
+### newsapi
+# sources = ["abc-news", "reuters"]
+
 # for source in sources:
 
-<<<<<<< HEAD
-sources = ["reuters"]
-store_dir = "/home/will/Projects/headliner/datastore/raw/"
-=======
-#     back_dates = pd.date_range(start="2020-01-14", end="2020-01-16")
->>>>>>> add-nyt-collector
+#     back_dates = pd.date_range(start="2020-01-22", end="2020-01-24")
 
 #     for back_date in back_dates:
 #         print(back_date)
 #         get_newsapi_on_date(back_date, intervals=12, source=source)
 #         process_source_on_date(back_date, source=source)
 
-<<<<<<< HEAD
-    back_dates = pd.date_range(start="2020-01-22", end="2020-01-24")
-
-    for back_date in back_dates:
-        print(back_date)
-        get_newsapi_on_date(back_date, intervals=12, source=source)
-        process_source_on_date(back_date, source=source)
-
-
-sources = ["abc-news"]
-store_dir = "/home/will/Projects/headliner/datastore/raw/"
-
-for source in sources:
-
-    back_dates = pd.date_range(start="2020-01-17", end="2020-01-24")
-=======
->>>>>>> add-nyt-collector
-
-back_dates = pd.date_range(start="2020-01-12", end="2020-01-22")
+### nyt api
+back_dates = pd.date_range(start="2020-01-23", end="2020-01-23")
 
 for back_date in back_dates:
     print(back_date)

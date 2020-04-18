f0cf4e8218e0f1d4d612d90e631ea5bc18f23384
Will Koeppen <will@axiomdatascience.com>
2020-04-18 13:36:08 -0800

    update nyt processor

--- headliner/headline_backfill.py
+++ headliner/headline_backfill.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 from headline_collector import get_newsapi_on_date
 from headline_processor import process_source_on_date
+from nyt_headline_processor import process_nytsource_on_date
 from nyt_headline_collector import get_nyt_on_date
 import glob
 import os
@@ -22,8 +23,10 @@ import pandas as pd
 #         process_source_on_date(back_date, source=source)
 
 ### nyt api
-back_dates = pd.date_range(start="2020-01-23", end="2020-01-23")
+back_dates = pd.date_range(start="2020-01-22", end="2020-04-16")
 
+# get_nyt_on_date(datetime(2020,1,29,0,0))
+#
 for back_date in back_dates:
     print(back_date)
-    get_nyt_on_date(back_date)
+    process_nytsource_on_date(back_date)

12a9737af71fe6a6d579cd66a3d58576492a0206
Will Koeppen <will@axiomdatascience.com>
2020-09-16 09:41:02 -0800

    update environment file

--- headliner/headline_backfill.py
+++ headliner/headline_backfill.py
@@ -1,6 +1,7 @@
 #!/home/will/anaconda3/envs/headliner/bin/python
 
 from datetime import datetime, timedelta
+import logging
 from headline_collector import get_newsapi_on_date
 from headline_processor import process_source_on_date
 from nyt_headline_processor import process_nytsource_on_date
@@ -9,24 +10,30 @@ import glob
 import os
 import pandas as pd
 
+logging.basicConfig(
+    filename='/home/will/Projects/headliner/headliner.log',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+    )
+logger = logging.getLogger(__name__)
 
-### newsapi
-# sources = ["abc-news", "reuters"]
+### Get news api sources
+newsapi_sources = ["cnn"]
 
-# for source in sources:
+for source in newsapi_sources:
 
-#     back_dates = pd.date_range(start="2020-01-22", end="2020-01-24")
+    back_dates = pd.date_range(start="2020-01-01", end="2020-01-14")
 
-#     for back_date in back_dates:
-#         print(back_date)
-#         get_newsapi_on_date(back_date, intervals=12, source=source)
-#         process_source_on_date(back_date, source=source)
+    for back_date in back_dates:
+        logger.info(f"Retrieving {source} from newsapi.org")
+        get_newsapi_on_date(back_date, intervals=12, source=source)
+        process_source_on_date(back_date, source=source)
 
 ### nyt api
-back_dates = pd.date_range(start="2020-04-16", end="2020-04-19")
+# back_dates = pd.date_range(start="2020-04-16", end="2020-04-19")
 
-get_nyt_on_date(datetime(2020, 4, 17, 0, 0))
-#
-for back_date in back_dates:
-    print(back_date)
-    process_nytsource_on_date(back_date)
+# get_nyt_on_date(datetime(2020, 4, 17, 0, 0))
+# #
+# for back_date in back_dates:
+#     print(back_date)
+#     process_nytsource_on_date(back_date)
